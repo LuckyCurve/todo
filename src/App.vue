b72cf5203d5ae98224cacc12c11569e3ce113259
luckycurve <luckycurvec@gmail.com>
2023-10-01 00:19:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoList from "@/components/TodoList.vue";
+import {onMounted} from "vue";
+
+onMounted(() => {
+  // document.documentElement.classList.add('dark')
+  console.log("asd");
+})
+</script>
+
+<template>
+  <todo-list/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,68 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const localStageName = 'todo-localStage';
+let info = ref('');
+let list = ref(localStorage.getItem(localStageName) == null ? [] : JSON.parse(localStorage.getItem(localStageName)));
+
+function addItem() {
+  if (info.value !== '') {
+    list.value.push({"value": info.value, "checked": false});
+    info.value = '';
+  } else {
+    alert('输入框不得为空')
+  }
+}
+
+function deleteItem(item) {
+  list.value = list.value.filter((t) => t !== item)
+}
+
+watch(list.value, (newValue, _) => {
+  console.log("123")
+  localStorage.setItem(localStageName, JSON.stringify(list.value))
+})
+
+watch(list, (newValue, _) => {
+  console.log("456")
+  localStorage.setItem(localStageName, JSON.stringify(list.value))
+})
+</script>
+
+<template>
+  <div class="body">
+    <h1>TODO-LIST</h1>
+    <div>
+      <input type="text" v-model="info">
+      <button @click="addItem">添加</button>
+    </div>
+    <ul>
+      <li class="item" v-for="item in list">
+        <input type="checkbox" v-model="item.checked">
+        <span :class="{finish: item.checked}">{{ item.value }}</span>
+        <input type="button" value="X" @click="deleteItem(item)">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  color: #6499E9;
+}
+
+.body {
+  background-color: antiquewhite;
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  margin: 20px 20px;
+  border: red dashed;
+}
+
+.finish {
+  text-decoration: line-through;
+}
+
+</style>
